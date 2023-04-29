5e44cdb3527d8f7d8e68511499ce2094c4158fcf
stagvant <131619524+stagvant@users.noreply.github.com>
2023-04-29 10:57:19 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ procrw(0x01BE2E32,BB100G,3,1);
 procrw(0x01BE305B,BB100H,3,1);
 procrw(0x01755076,BB100J,2,1);
 procrw(0x01FA10CF,BB100K,2,1);
-procrw(0x01FE3C87,BB100L,1,1);
+procrw(0x019154C8,BB100L,1,1);
 procrw(0x019176D3,BB100M,1,1);
 procrw(0x00FB15D5,BB100N,1,1);
 procrw(0x00FB15E5,BB100O,1,1);
@@ -37,6 +37,10 @@ procrw(0x01F84699,BB100P,1,1);
 procrw(0x01D36313,BB100Q,1,1);
 procrw(0x00F7002D,BB100R,1,1);
 procrw(0x0220C2FC,BB100S,1,1);
+procrw(0x01FE3DF3,BB100T,13,1);
+procrw(0x01FE3C74,BB100U,35,1);
+procrw(0x01915461,BB100V,63,1);
+procrw(0x01914B54,BB100W,200,1);
 }else{
 procrw(0x01C3A77F,verid,1,0);
 if(verid[0]==(unsigned char)9){
@@ -51,13 +55,15 @@ procrw(0x01C3A9EB,BB109H,3,1);
 procrw(0x01771E86,BB109J,2,1);
 procrw(0x02038885,BB109K,2,1);
 procrw(0x0193F73E,BB109L,1,1);
-procrw(0x0207A447,BB109L,1,1);
 procrw(0x00FB9355,BB109M,1,1);
 procrw(0x00FB9365,BB109N,1,1);
 procrw(0x0201C059,BB109O,1,1);
 procrw(0x01D9DEC3,BB109P,1,1);
 procrw(0x00F7190D,BB109Q,1,1);
 procrw(0x022A457B,BB109R,1,1);
+procrw(0x0207A5B3,BB109S,13,1);
+procrw(0x0207A434,BB109T,35,1);
+procrw(0x0193C10C,BB109U,208,1);
 }else{
 procrw(0x01C3AACF,verid,1,0);
 if(verid[0]==(unsigned char)6){
@@ -72,13 +78,15 @@ procrw(0x01C3AD3B,BB106H,3,1);
 procrw(0x017720A6,BB106J,2,1);
 procrw(0x02039BD5,BB106K,2,1);
 procrw(0x0193FB7E,BB106L,1,1);
-procrw(0x0207B837,BB106M,1,1);
-procrw(0x00FB9625,BB106N,1,1);
-procrw(0x00FB9635,BB106O,1,1);
-procrw(0x0201D409,BB106P,1,1);
-procrw(0x01D9E0D3,BB106Q,1,1);
-procrw(0x00F71BDD,BB106R,1,1);
-procrw(0x022A524B,BB106S,1,1);}}}
+procrw(0x00FB9625,BB106M,1,1);
+procrw(0x00FB9635,BB106N,1,1);
+procrw(0x0201D409,BB106O,1,1);
+procrw(0x01D9E0D3,BB106P,1,1);
+procrw(0x00F71BDD,BB106Q,1,1);
+procrw(0x022A524B,BB106R,1,1);
+procrw(0x0207B9A3,BB106S,13,1);
+procrw(0x0207B824,BB106T,35,1);
+procrw(0x0193C54C,BB106U,208,1);}}}
 scePthreadExit(NULL);
 return NULL;}
 int32_t attr_module_hidden module_start(size_t argc,const void *args){
